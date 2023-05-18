1f2308e08333cbeeb41c7134e57f5678676e7393
Przemysław Rudzinski <przemyslaw.rudzinski@nextlevel.studio>
2023-05-18 16:34:43 +0200

    nowe-style

--- scoreboard/src/App.js
+++ scoreboard/src/App.js
@@ -1,100 +1,94 @@
-import React,  {useState,useEffect}  from 'react';
-import xtype from 'xtypejs'
-import './App.css';
-import Player from './components/player';
-import Players from './components/players';
-import Score from './components/score';
-
-
-
+import React, { useState, useEffect } from "react";
+import xtype from "xtypejs";
+import "./App.css";
+import Player from "./components/player";
+import Players from "./components/players";
+import Score from "./components/score";
 
 function App() {
-
   const [dataScore, setScoreData] = useState([]);
   const [dataHome, setHomeData] = useState([]);
   const [dataGuest, setGuestData] = useState([]);
   const [scoreHome, setHomeScore] = useState([]);
   const [scoreGuest, setGuestScore] = useState([]);
 
-  const getScoreData=()=>{
-    fetch('http://localhost:8000/score'
-    ,{
-      headers : {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    )
-      .then(function(response){
+  const getScoreData = () => {
+    fetch("http://localhost:8000/score", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
         // console.log(response)
         return response.json();
-      }).catch(error => console.error('Error:', error))
-      .then(response => {
-        // console.log('Success:', JSON.stringify(response))
-        setScoreData(response)
       })
-  }
-  useEffect(()=>{
-    getScoreData()
-  }, [])
+      .catch((error) => console.error("Error:", error))
+      .then((response) => {
+        // console.log('Success:', JSON.stringify(response))
+        setScoreData(response);
+      });
+  };
+  useEffect(() => {
+    getScoreData();
+  }, []);
 
-  const getPlayersHomeData=()=>{
-    fetch('http://localhost:8000/players_home'
-    ,{
-      headers : {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    )
-      .then(function(response){
+  const getPlayersHomeData = () => {
+    fetch("http://localhost:8000/players_home", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
         // console.log(response)
         return response.json();
-      }).catch(error => console.error('Error:', error))
-      .then(response => {
-        // console.log('Success:', JSON.stringify(response))
-        let score_home = 0
-        response?.map(player => {
-          score_home += parseInt(player.goals, 10)
-        })
-        console.log("getHomeScore", score_home)
-        // console.log('Success:', JSON.stringify(response))
-        setHomeScore(score_home)
-        setHomeData(response)
       })
-  }
-  useEffect(()=>{
-    getPlayersHomeData()
-  }, [dataHome])
+      .catch((error) => console.error("Error:", error))
+      .then((response) => {
+        // console.log('Success:', JSON.stringify(response))
+        let score_home = 0;
+        response?.map((player) => {
+          score_home += parseInt(player.goals, 10);
+        });
+        console.log("getHomeScore", score_home);
+        // console.log('Success:', JSON.stringify(response))
+        setHomeScore(score_home);
+        setHomeData(response);
+      });
+  };
+  useEffect(() => {
+    getPlayersHomeData();
+  }, [dataHome]);
 
-  const getPlayersGuestsData=()=>{
-    fetch('http://localhost:8000/players_guests'
-    ,{
-      headers : {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-       }
-    }
-    ).then(function(response){
-      // console.log(response)
-      return response.json();
-      }).catch(error => console.error('Error:', error))
-      .then(response => {
+  const getPlayersGuestsData = () => {
+    fetch("http://localhost:8000/players_guests", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then(function (response) {
+        // console.log(response)
+        return response.json();
+      })
+      .catch((error) => console.error("Error:", error))
+      .then((response) => {
         // console.log(response);
         // console.log('Success:', JSON.stringify(response))
-        let score_guest = 0
-        response?.map(player => {
-          score_guest += parseInt(player.goals, 10)
-        })
-        console.log("getHomeScore", score_guest)
+        let score_guest = 0;
+        response?.map((player) => {
+          score_guest += parseInt(player.goals, 10);
+        });
+        console.log("getHomeScore", score_guest);
         // console.log('Success:', JSON.stringify(response))
-        setGuestScore(score_guest)
-        setGuestData(response)
-      })
-  }
-  useEffect(()=>{
-    getPlayersGuestsData()
-  },[dataGuest])
+        setGuestScore(score_guest);
+        setGuestData(response);
+      });
+  };
+  useEffect(() => {
+    getPlayersGuestsData();
+  }, []);
 
   // handleIncrementExclusionsHome = (player) => {
   //   const players_home = [...this.state.players_home];
@@ -154,7 +148,6 @@ function App() {
   //   calcScore(dataHome)
   // }, []);
 
-
   return (
     <React.Fragment>
       <main className="container">
@@ -170,33 +163,35 @@ function App() {
             <div className="col-md-6">
               <Player
                 key={0}
-                player={{num: "Nr", name: "Imię", surname: "Nazwisko", exclusions: "W", goals: "G"}}
+                player={{
+                  num: "Nr",
+                  name: "Imię",
+                  surname: "Nazwisko",
+                  exclusions: "W",
+                  goals: "G",
+                }}
               />
-              {dataHome?.map(player =>
-                <Player
-                  key={player.num}
-                  player={player}
-                />
-              )}
+              {dataHome?.map((player) => (
+                <Player key={player.num} player={player} />
+              ))}
             </div>
             <div className="col-md-6">
-            <Player
-              key={0}
-              player={{num: "Nr", name: "Imię", surname: "Nazwisko", exclusions: "W", goals: "G"}}
-            />
-            {dataGuest?.map(player =>
-                <Player
-                  key={player.num}
-                  player={player}
-                />
-              )}
+              <Player
+                key={0}
+                player={{
+                  num: "Nr",
+                  name: "Imię",
+                  surname: "Nazwisko",
+                  exclusions: "W",
+                  goals: "G",
+                }}
+              />
+              {dataGuest?.map((player) => (
+                <Player key={player.num} player={player} />
+              ))}
             </div>
           </div>
-
-
         </div>
-
-
       </main>
     </React.Fragment>
   );
--- scoreboard/src/components/score.jsx
+++ scoreboard/src/components/score.jsx
@@ -1,14 +1,24 @@
-import React, { Component } from 'react';
-import '../assets/styles/score.css'
+import React, { Component } from "react";
+import "../assets/styles/score.css";
 class Score extends Component {
   // state = {
   //   scoreHome_state: 0,
   //   scoreGuest_state: 0,
   // }
 
-
   render() {
-    const { score_home, exc_home, to_home, score_guests, exc_guests, to_guests, time_minutes, time_seconds, action_time, quarter } = this.props.score;
+    const {
+      score_home,
+      exc_home,
+      to_home,
+      score_guests,
+      exc_guests,
+      to_guests,
+      time_minutes,
+      time_seconds,
+      action_time,
+      quarter,
+    } = this.props.score;
     // const { dataGuest } = this.props.dataguest
 
     // const calcScore = (data) => {
@@ -19,37 +29,44 @@ class Score extends Component {
     //   console.log(dataHome)
     //   return sum_score
     // }
-
     return (
       <div className="row">
         <div className="col-md-4">
           <div className="row">
             <div className="col-md-6">
-              <img src={require('../assets/AZS-Uniwersytet-Warszawski.png')} />
-            </div>
-              <div className="col-md-6">
-              <div className="row utilL">{this.props.scorehome}</div>
-              {exc_home === 0 ? null : <div className="row utilL">exc: {exc_home}</div>}
-              <div className="row utilL">Timeouts left: {to_home}</div>
+              <img src={require("../assets/AZS-Uniwersytet-Warszawski.png")} />
             </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="timer">
-            {/* <div className="row utilL">{time_minutes}:{time_seconds}</div>
-            <div className="row utilL">:{action_time}</div> */}
-            <div className="row utilL">Q{quarter}</div>
+            <div className="utilL">
+              <h1>{this.props.scorehome}</h1>
+              <h1>Q{quarter}</h1>
+              <h1>{this.props.scoreguest}</h1>
+            </div>
+            <div className="timeout-wrapper">
+              <div className="col-md-6">
+                {exc_home === 0 ? null : (
+                  <div className="util-timeout">exc: {exc_home}</div>
+                )}
+                <div className="util-timeout">Timeouts left: {to_home}</div>
+              </div>
+              <div className="col-md-6">
+                {exc_guests === 0 ? null : (
+                  <div className="util-timeout">exc: {exc_guests}</div>
+                )}
+                <div className="util-timeout">Timeouts left: {to_guests}</div>
+              </div>
+            </div>
           </div>
         </div>
         <div className="col-md-4">
           <div className="row">
-              <div className="col-md-6">
-              <div className="row utilL">{this.props.scoreguest}</div>
-              {exc_guests === 0 ? null : <div className="row utilL">exc: {exc_guests}</div>}
-              <div className="row utilL">Timeouts left: {to_guests}</div>
-            </div>
-            <div className="col-md-6">
-              <img src={require('../assets/arkonia_szczecin-removebg-preview.png')} />
+            <div className="col-md-6 ml-auto">
+              <img
+                src={require("../assets/arkonia_szczecin-removebg-preview.png")}
+              />
             </div>
           </div>
         </div>
@@ -57,10 +74,7 @@ class Score extends Component {
     );
   }
 
-  formatExclusions() {
-
-  }
+  formatExclusions() {}
 }
-
 
 export default Score;
