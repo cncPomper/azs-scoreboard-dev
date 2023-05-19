90d0b437e228b0c886133ad0950c5acaa2031bda
Przemysław Rudzinski <przemyslaw.rudzinski@nextlevel.studio>
2023-05-19 15:11:52 +0200

    side-pannel-styling

--- side-panel/src/App.js
+++ side-panel/src/App.js
@@ -1,23 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import Team from './Team.js';
-import './App.css';
-import Player from './Player.js';
-import apiRequest from './apiRequest.js';
-import Header from './Header.js';
-import Nav from './Nav.js';
-import Content from './Content.js';
-import PlayerDetails from './PlayerDetails.js';
-import NewPlayer from './NewPlayer.js';
-import EditPlayer from './EditPlayer.js';
-import { Route, Routes, useNavigate } from 'react-router-dom';
-import api from './api/players';
+import React, { useState, useEffect } from "react";
+import Team from "./Team.js";
+import "./App.css";
+import Player from "./Player.js";
+import apiRequest from "./apiRequest.js";
+import Header from "./Header.js";
+import Nav from "./Nav.js";
+import Content from "./Content.js";
+import PlayerDetails from "./PlayerDetails.js";
+import NewPlayer from "./NewPlayer.js";
+import EditPlayer from "./EditPlayer.js";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import api from "./api/players";
 
 function App() {
+  const API_URL = "http://localhost:8000";
 
-  const API_URL = 'http://localhost:8000';
-
-  const [homeTeamName, setHomeTeamName] = useState('players_home')
-  const [guestsTeamName, setGuestsTeamName] = useState('players_guests')
+  const [homeTeamName, setHomeTeamName] = useState("players_home");
+  const [guestsTeamName, setGuestsTeamName] = useState("players_guests");
   const [teamHome, setTeamHome] = useState([]);
   const [teamGuests, setTeamGuests] = useState([]);
   const [fetchError, setFetchError] = useState(null);
@@ -27,41 +26,40 @@ function App() {
     name: "Imię",
     surname: "Nazwisko",
     exclusions: "W",
-    goals: "G"
-  })
+    goals: "G",
+  });
 
-  const navigate = useNavigate ();
+  const navigate = useNavigate();
 
   const [editNum, setEditNum] = useState(0);
-  const [editName, setEditName] = useState('');
-  const [editSurname, setEditSurname] = useState('');
+  const [editName, setEditName] = useState("");
+  const [editSurname, setEditSurname] = useState("");
   const [editGoals, setEditGoals] = useState(0);
   const [editExclusions, setEditExclusions] = useState(0);
-
 
   useEffect(() => {
     const fetchTeam = async (teamSide) => {
       try {
         const response = await api.get(`/${teamSide}`);
         if (teamSide === homeTeamName) {
-          setTeamHome(response.data)
+          setTeamHome(response.data);
         } else {
-          setTeamGuests(response.data)
+          setTeamGuests(response.data);
         }
       } catch (error) {
         if (error.response) {
-           // Not inside 200 response range
-          console.log(error.response.data)
-          console.log(error.response.status)
-          console.log(error.response.headers)
+          // Not inside 200 response range
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
         }
       } finally {
         setIsLoading(false);
       }
-    }
+    };
     fetchTeam(homeTeamName);
     fetchTeam(guestsTeamName);
-  }, [])
+  }, []);
 
   const handleEdit = async (id) => {
     const updatedPlayer = {
@@ -70,34 +68,41 @@ function App() {
       name: editName,
       surname: editSurname,
       goals: editGoals,
-      exclusions: editExclusions
-    }
+      exclusions: editExclusions,
+    };
     try {
       if (parseInt(id, 10) >= 0 && parseInt(id, 10) <= 12) {
         const response = await api.put(`/${homeTeamName}/${id}`, updatedPlayer);
-        setTeamHome(teamHome.map(player => player.id === id ? { ...response.data } : player))
+        setTeamHome(
+          teamHome.map((player) =>
+            player.id === id ? { ...response.data } : player
+          )
+        );
         setEditNum(0);
-        setEditName('');
-        setEditSurname('');
+        setEditName("");
+        setEditSurname("");
         setEditGoals(0);
         setEditExclusions(0);
-        navigate('/');
+        navigate("/");
       } else {
-        const response = await api.put(`/${guestsTeamName}/${id}`, updatedPlayer);
-        setTeamGuests(teamGuests.map(player => player.id === id ? { ...response.data } : player))
+        const response = await api.put(
+          `/${guestsTeamName}/${id}`,
+          updatedPlayer
+        );
+        setTeamGuests(
+          teamGuests.map((player) =>
+            player.id === id ? { ...response.data } : player
+          )
+        );
         setEditNum(0);
-        setEditName('');
-        setEditSurname('');
+        setEditName("");
+        setEditSurname("");
         setEditGoals(0);
         setEditExclusions(0);
-        navigate('/');
+        navigate("/");
       }
-
-    } catch (error) {
-
-    }
-  }
-
+    } catch (error) {}
+  };
 
   // useEffect(() => {
   //   const fetchTeam = async (teamSide) => {
@@ -126,20 +131,22 @@ function App() {
 
   return (
     <div className="App">
-      <Header />
-      <Nav />
-      <Routes >
-
-        <Route exact path='/' element={
-          <Content
-            isLoading={isLoading}
-            fetchError={fetchError}
-            playerTemplate={playerTemplate}
-            teamHome={teamHome}
-            teamGuests={teamGuests}
-          />
-        }>
-        </Route>
+      {/* <Header />
+      <Nav /> */}
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={
+            <Content
+              isLoading={isLoading}
+              fetchError={fetchError}
+              playerTemplate={playerTemplate}
+              teamHome={teamHome}
+              teamGuests={teamGuests}
+            />
+          }
+        ></Route>
 
         {/* <Route exact path='/player' element={
           <NewPlayer
@@ -150,33 +157,33 @@ function App() {
         >
         </Route> */}
 
-        <Route path="/edit/:id" element={
-          <EditPlayer
-            teamHome={teamHome}
-            teamGuests={teamGuests}
-            handleEdit={handleEdit}
-            editNum={editNum}
-            setEditNum={setEditNum}
-            editName={editName}
-            setEditName={setEditName}
-            editSurname={editSurname}
-            setEditSurname={setEditSurname}
-            editGoals={editGoals}
-            setEditGoals={setEditGoals}
-            editExclusions={editExclusions}
-            setEditExclusions={setEditExclusions}
-          />
+        <Route
+          path="/edit/:id"
+          element={
+            <EditPlayer
+              teamHome={teamHome}
+              teamGuests={teamGuests}
+              handleEdit={handleEdit}
+              editNum={editNum}
+              setEditNum={setEditNum}
+              editName={editName}
+              setEditName={setEditName}
+              editSurname={editSurname}
+              setEditSurname={setEditSurname}
+              editGoals={editGoals}
+              setEditGoals={setEditGoals}
+              editExclusions={editExclusions}
+              setEditExclusions={setEditExclusions}
+            />
           }
-        >
-        </Route>
+        ></Route>
 
-        <Route path='/player/:id' element={
-          <PlayerDetails
-            teamHome={teamHome}
-            teamGuests={teamGuests}
-          />}>
-        </Route>
-
+        <Route
+          path="/player/:id"
+          element={
+            <PlayerDetails teamHome={teamHome} teamGuests={teamGuests} />
+          }
+        ></Route>
       </Routes>
     </div>
   );
